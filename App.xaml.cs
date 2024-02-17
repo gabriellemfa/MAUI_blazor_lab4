1d3102957c4a4b702c6fb0ca7d8b3caf7004cd2e
amxgielle <152216187+amxgielle@users.noreply.github.com>
2024-02-16 22:18:13 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace lab4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
